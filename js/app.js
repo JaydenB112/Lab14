e7e44f425b52bda9d5f049f208182605775c2569
Jayden Boyd <jaydenboydb1@gmail.com>
2023-03-23 15:21:28 -0500

    I dun it Forest -Bubba

--- js/app.js
+++ js/app.js
@@ -20,7 +20,8 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  localStorage("cart-contents",)
+  const myJSON = JSON.stringify(Cart.items);
+  localStorage.setItem("cart-contents",myJSON)
 };
 
 Cart.prototype.removeItem = function(item) {
