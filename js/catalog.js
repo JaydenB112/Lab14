ec10e281ccd8b1c563439e917ec13caaeb0134ea
Daron Faulkner Jr <dj200099@gmail.com>
2023-03-29 10:17:45 -0500

    preview cart done

--- js/catalog.js
+++ js/catalog.js
@@ -49,6 +49,9 @@ function addSelectedItemToCart() {
     }
   }
 
+  let quantityHTML = document.getElementById("quantity")
+  let quantity = quantityHTML.value
+state.cart.addItem(productObject, quantity)
 
 }
 
@@ -63,8 +66,8 @@ function updateCartPreview() {
 
   // for(let i = 0; i < state.allProducts.length; i++){
   // }
-preview.innerHTML = '${itemQuantity} ${itemName}';
-  document.getElementById('cartContents').append()
+preview.innerHTML = "Item " + itemName + " Quantity " + itemQuantity;
+  document.getElementById('cartContents').append(preview)
 }
 
 // Set up the "submit" event listener on the form.
